8aa2f171d76c172eccba6c2cd01126542c4d8fe5
Alexander Simmerl <a.simmerl@gmail.com>
2019-08-02 14:14:42 +0200

    Remove dead code

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,6 @@ import App, { Container } from 'next/app'
 import Head from 'next/head'
 
 export default class OscoinApp extends App {
-  // static async getInitialProps({ Component, ctx }) {
-  //   let pageProps = {}
-
-  //   if (Component.getInitialProps) {
-  //     pageProps = await Component.getInitialProps(ctx)
-  //   }
-
-  //   return { pageProps }
-  // }
-
   render() {
     const { Component, pageProps } = this.props
 
