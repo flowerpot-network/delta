298a9fc9e378e5a986cf612fe48daf7437bd78d8
Julien Donck <donckjulien@gmail.com>
2019-08-19 18:05:14 +0200

    more styling

--- pages/index.js
+++ pages/index.js
@@ -31,71 +31,71 @@ export default function Index() {
         <table>
           <tr>
             <th>Topic</th>
-            <th>Content</th>
+            {/* <th>Content</th> */}
             <th>Lecturer</th>
             <th>Date</th>
           </tr>
           <tr>
             <td>Intro to P2P</td>
-            <td>
+            {/* <td>
               Peer to peer systems are increasingly moving from the periphery toward the center of how we create and, just as importantly, distribute value.
-            </td>
+            </td> */}
             <td>Stacco Troncoso</td>
             <td>4 Nov 2019</td>
           </tr>
           <tr>
             <td>Primitives</td>
-            <td>
+            {/* <td>
               Hash functions & chaining, symmetric/asymmetric/multi/ring signatures, merkle trees, DHTs, RSMs
-            </td>
+            </td> */}
             <td>Lucas Vogelsang</td>
             <td>6 Nov 2019</td>
           </tr>
           <tr>
             <td>Social P2P</td>
-            <td>
+            {/* <td>
               Scuttlebutt, messaging, communities and users
-            </td>
+            </td> */}
             <td>Sarah Friend</td>
             <td>11 Nov 2019</td>
           </tr>
           <tr>
             <td>Consensus Mechanisms</td>
-            <td>
+            {/* <td>
               Consensus mechanisms: Bitcoin/Nakamoto consensus, BFT
-            </td>
+            </td> */}
             <td>Alexis Sellier</td>
             <td>13 Nov 2019</td>
           </tr>
           <tr>
             <td>Smart Contracts</td>
-            <td>
+            {/* <td>
               Programmable money, legal implications, Ethereum (solidity)
-            </td>
+            </td> */}
             <td></td>
             <td>18 Nov 2019</td>
           </tr>
           <tr>
             <td>Security</td>
-            <td>
+            {/* <td>
               Key management, smart contract best practices, general secure code frameworks?
-            </td>
+            </td> */}
             <td>Liz Steininger</td>
             <td>20 Nov 2019</td>
           </tr>
           <tr>
             <td>Mechanism Design</td>
-            <td>
+            {/* <td>
               Trust models, incentives, game theory
-            </td>
+            </td> */}
             <td>Ele Diakomichalis</td>
             <td>25 Nov 2019</td>
           </tr>
           <tr>
             <td>Decentralized Governance</td>
-            <td>
+            {/* <td>
               Who has power? Stakeholders, on & off-chain governance; DAOs
-            </td>
+            </td> */}
             <td>Gnosis + DAOStack</td>
             <td>27 Nov 2019</td>
           </tr>
