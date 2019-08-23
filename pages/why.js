c0413beb8da14b0e348b72e329d45112416913a7
abbey <abbeytitcomb@gmail.com>
2019-08-23 11:21:55 +0200

    abbey edits

--- pages/why.js
+++ pages/why.js
@@ -5,19 +5,20 @@ export default function Index() {
     <Page>
       <div className="content">
         <h1>
-        Peer to peer technologies are changing our world - we need to make sure the right people are getting involved.
+        Peer-to-peer technologies are changing our world.
         </h1>
-        <h3>
-          Peer to Peer (P2P) Architecture: From 1979 to today
-        </h3>
         <p>
-          P2P defines an architecture that enables sharing tasks or workloads between peers. P2P systems like &quot;Blockchain,&quot; &quot;Web3.0,&quot; and &quot;decentralized systems&quot; have dominated news and conversations for the last few years. But P2P systems have existed for decades, in many shapes and forms. Starting with Usenet (a worldwide distributed discussion system available on computers) in 1979, this kind of peer-to-peer resource exchange was popularized by Napster and file sharing in 1999. Today, we see P2P technologies challenging centralized web services, P2P philosophies subverting business models and governance structures, and P2P culture demanding greater support of &quot;the commons,&quot; like free and open source software.
+        P2P systems like &quot;Blockchain,&quot; &quot;Web3.0,&quot; and &quot;decentralized systems&quot; have dominated news and conversations for the last few years. But P2P systems have existed for decades, in many shapes and forms. Starting with Usenet (a worldwide distributed discussion system available on computers) in 1979, this kind of peer-to-peer resource exchange was popularized by Napster and file sharing in 1999. Today, we see P2P technologies challenging centralized web services, P2P philosophies subverting business models and governance structures, and P2P culture demanding greater support of &quot;the commons,&quot; like free and open source software.
         </p>
         <h3>
-          AccessP2P: An introduction to the technical stack and community behind peer-to-peer (P2P) technologies
+        We want to make P2P learning more accessible.
         </h3>
         <p>
-          While a few examples of peer-to-peer technologies have dominated the news, there seem to be few resources for getting an overview of the P2P technology stack and community involved. We believe this creates unnecessary challenges for individuals to get involved - and as P2P technologies gain traction, it becomes more and more important that we get the right people building the technologies. We created AccessP2P in order to build an open-sourced, publicly-accessible library of materials for learning about fundamental P2P concepts. By bringing together members of the P2P community, we will explore many technical topics at a high level in an effort to give early-in-career developers the knowledge and network opportunities they need to better explore issues and careers in the P2P space. While this will be a free program and lectures will take place in-person in Berlin, all materials and output from the lectures will be made publicly available online.
+           While a few examples of peer-to-peer technologies have dominated the news, there seem to be few resources for getting an overview of the P2P technology stack and community involved. We believe this creates unnecessary challenges for individuals to get involved - and as P2P technologies gain traction, it becomes more and more important that we get the right people building the technologies.
+        </p>
+        <br />
+        <p>
+           We created AccessP2P in order to build an <b> open-sourced, publicly-accessible </b> library of materials for learning about fundamental P2P concepts. By bringing together members of the P2P community, we will explore many technical topics at a high level in an effort to give early-in-career developers the knowledge and network opportunities they need to better explore issues and careers in the P2P space. We  While this will be a free program and lectures will take place in-person in Berlin, all materials and output from the lectures will be made publicly available online.
         </p>
         <h3>
           Our Priority: Diversity and access for all
