a873916ecafb462f519de458eea68c4305de385b
Julien Donck <donckjulien@gmail.com>
2019-08-29 22:01:38 +0200

    faq+sub

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,19 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer>
-      For more information, read our <Link href="/faq"><a>FAQ</a></Link> or feel free to <a href="mailto:ange@monadic.xyz">reach out</a>.
-
+      <br/>
+      <p>
+        Are you interested in the program and content but not ready or able to participate? Sign up for updates from our organizers.
+      </p>
+      <br/>
+      <a className="subscribe button" href="https://accessp2p.typeform.com/to/J4ahyX" target="_blank" rel="noopener noreferrer">
+        Stay informed
+      </a>
+      <br/>
+      <br/>
+      <br/>
+      <p>
+        For more information, read our <Link href="/faq"><a>FAQ</a></Link> or feel free to <a href="mailto:ange@monadic.xyz">reach out</a>.
+      </p>
     </footer>)
 }
--- pages/faq.js
+++ pages/faq.js
@@ -9,9 +9,22 @@ const Content = () => {
         </h1>
         <br/>
         <br/>
-        <p>
-          This is where the FAQ will go
-        </p>
+        <div>
+          <p className="question">
+            Isn&apos;t this the most amazing questions?
+          </p>
+          <p className="answer">
+            It is a really good question.
+          </p>
+        </div>
+        <div>
+          <p className="question">
+            Isn&apos;t this the most amazing questions?
+          </p>
+          <p className="answer">
+            Aliquip id excepteur elit aliqua ad. Nisi deserunt in excepteur exercitation qui commodo esse dolore sint eiusmod. Pariatur mollit qui amet commodo esse minim cupidatat duis. Ipsum minim veniam magna et elit ex laborum labore sit ut mollit duis. Dolore adipisicing qui officia non qui eu ipsum et anim. Dolor ullamco excepteur pariatur magna do laborum cillum duis sit adipisicing dolor sint. Officia irure nisi exercitation nostrud aute exercitation laboris anim velit non nisi.
+          </p>
+        </div>
       </div>
     </Page>
   )
