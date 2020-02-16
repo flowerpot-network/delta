196b7d54fa78e01a80f831e84cbd7f4342243c7e
abbey <abbeytitcomb@gmail.com>
2020-02-16 03:22:49 -0700

    great work

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,27 @@ import Layout from "../components/Layout";
 
 export default () => (
   <Layout>
-    <h1 className="text-5xl font-bold">Growing open source 🌞</h1>
+  <div className="">
+    <h1 className="text-5xl font-bold">Growing open source together 🌞</h1>
     <h3 className="text-2xl">Free and open source GitHub sponsors</h3>
+    </div>
+  <div className="text-xl mt-10">
+    <p>Flowerpots are Ethereum wallets connected to GitHub orgs.</p>
+  </div>
+  <div className="text-xl mt-8">
+    <p className="mb-4"> Search for your org</p>
+    <code className="block bg-gray-900 text-gray-200 rounded p-4 text-sm">
+      [![Sponsor me]
+    </code>
+  </div>
+  <div className="text-xl mt-8">
+    <p> Generate a markdown button for your README.md that connects to Metamask.</p>
+  </div>
+  <div className="text-xl mt-8">
+    <p> Generate a markdown button for your README.md that connects to Metamask.</p>
+  </div>
+  <div className="text-xl mt-8">
+    <p> Claim your Org, generate your button, and start receiving crypto donations without the overhead </p>
+  </div>
   </Layout>
 );
