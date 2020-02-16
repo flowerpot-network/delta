6b8322e345570d8d01270d4ab66233f05d026d11
abbey <abbeytitcomb@gmail.com>
2020-02-16 03:33:02 -0700

    blah

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,16 @@ export default () => (
     <p>Flowerpots are Ethereum wallets connected to GitHub orgs.</p>
   </div>
   <div className="text-xl mt-8">
-    <p className="mb-4"> Search for your org</p>
+    <p className="mb-4"> Step 1: Search for your org</p>
     <code className="block bg-gray-900 text-gray-200 rounded p-4 text-sm">
-      [![Sponsor me]
+      https://flowerpot.network/[YOUR_ORG_NAME]
     </code>
+  </div>
+  <div className="text-xl mt-8">
+    <p className="mb-4"> Step 2: Activate your profile & link GitHub</p>
+  </div>
+  <div className="text-xl mt-8">
+    <p className="mb-4"> Step 3: Create a new wallet for your GitHub org (i.e. Gnosis safe, Metamask) </p>
   </div>
   <div className="text-xl mt-8">
     <p> Generate a markdown button for your README.md that connects to Metamask.</p>
