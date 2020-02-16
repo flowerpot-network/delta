eb7ae86d9f20aff8d271bce1591013664b89b9bb
Alex Miles <alex@tokentax.us>
2020-02-16 05:48:50 -0700

    STyle

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,15 @@ export default ({ children }) => {
     <div className="antialiased text-gray-900 max-w-4xl m-auto px-4 mt-8">
       <header className="my-3 mb-8 flex justify-between">
         <Link href="/">
-          <h1 className="text-2xl cursor-pointer font-semibold">Flowerpot 🌻</h1>
+          <h1 className="text-2xl cursor-pointer font-semibold">
+            🌻 Flowerpot
+          </h1>
         </Link>
         <nav>
-          <Link href="/account">
+          <Link
+            href="/
+          "
+          >
             <a className="text-blue-600">My Account </a>
           </Link>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,115 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout'
+import Link from 'next/link'
+
+const exampleOrgs = [
+  {
+    name: 'Ethereum',
+    image: 'https://avatars1.githubusercontent.com/u/6250754?s=200&v=4',
+    imageWidth: '80%'
+  },
+  {
+    name: 'Metamask',
+    image: 'https://avatars1.githubusercontent.com/u/11744586?s=200&v=4',
+    imageWidth: '50%'
+  },
+  {
+    name: '3Box',
+    image: 'https://avatars1.githubusercontent.com/u/45031480?s=200&v=4',
+    imageWidth: '50%'
+  },
+  {
+    name: 'Uniswap'
+  }
+]
 
 export default () => (
   <Layout>
-  <div className="">
-    <h1 className="text-5xl font-bold">Growing open source together 🌞</h1>
-    <h3 className="text-2xl">Free and open source GitHub sponsors</h3>
+    <div className="text-center mb-4 pb-16 lg:pb-24">
+      <span className="text-6xl text-center">✌️🌞✌️</span>
+      <h1 className="text-5xl font-bold text-center leading-tight mb-5">
+        Growing open source together
+      </h1>
+      <h3 className="text-2xl text-center text-gray-600">
+        Free and open source GitHub sponsors
+      </h3>
     </div>
-  <div className="text-xl mt-10">
-    <p>Flowerpots are Ethereum wallets connected to GitHub orgs.</p>
-  </div>
-  <div className="text-xl mt-8">
-    <p className="mb-4"> Step 1: Search for your org</p>
-    <code className="block bg-gray-900 text-gray-200 rounded p-4 text-sm">
-      https://flowerpot.network/[YOUR_ORG_NAME]
-    </code>
-  </div>
-  <div className="text-xl mt-8">
-    <p className="mb-4"> Step 2: Activate your profile & link GitHub</p>
-  </div>
-  <div className="text-xl mt-8">
-    <p className="mb-4"> Step 3: Create a new wallet for your GitHub org (i.e. <a href="https://safe.gnosis.io/multisig">a Gnosis safe</a> multisig or a <a href="https://metamask.io/">Metamask</a>) </p>
-  </div>
-  <div className="text-xl mt-8">
-    <p className="mb-4"> Step 4: Generate a markdown button for README.md's in your GitHub Org! </p>
-  </div>
-  <div className="container mt-16 flex justify-center mx-auto">
-    <section id="flowers">
-              <pre><code>                _(_)_                     _(_)_     </code></pre>
-              <pre><code>    @@@@       (_)@(_)        @@@@       (_)@(_)    </code></pre>
-              <pre><code>   @@()@@ wWWWw  (_)\        @@()@@ wWWWw  (_)\     </code></pre>
-              <pre><code>    @@@@  (___)     \|/       @@@@  (___)     \|/   </code></pre>
-              <pre><code>     /      Y       \|         /      Y       \|    </code></pre>
-              <pre><code>  \ |     \ |/       | /    \ |     \ |/       | /  </code></pre>
-              <pre><code>  \\|//   \\|///  \\\|//    \\|//   \\|///  \\\|//  </code></pre>
-              <pre><code>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^</code></pre>
-    </section>
-  </div>
+
+    <div>
+      <div className="flex -mx-1">
+        {exampleOrgs.map(org => (
+          <div className="border rounded w-1/3 flex-wrap mx-2 p-3">
+            {/* <div className="text-center">
+            <img src={org.image} className="mx-auto" width={org.imageWidth} />
+          </div> */}
+            <h2 className="font-bold">{org.name}</h2>
+            <Link href={`https://flowerpot.network/${org.name.toLowerCase()}`}>
+              Water Flowerpot 🚰
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="text-xl mt-10">
+      <p className="text-xl">
+        Flowerpots are Ethereum wallets connected to GitHub orgs.
+      </p>
+      <div className="text-xl mt-8">
+        <p className="mb-4">
+          <span className="font-bold">Step 1:</span> Search for your org
+        </p>
+        <code className="block bg-gray-900 text-gray-200 rounded p-4 text-sm">
+          https://flowerpot.network/[YOUR_ORG_NAME]
+        </code>
+      </div>
+      <div className="text-xl mt-8">
+        <p className="mb-4">
+          <span className="font-bold">Step 2:</span> Activate your profile &
+          link GitHub
+        </p>
+      </div>
+      <div className="text-xl mt-8">
+        <p className="mb-4">
+          <span className="font-bold">Step 3:</span> Create a new wallet for
+          your GitHub org (i.e.{' '}
+          <a href="https://safe.gnosis.io/multisig">a Gnosis safe</a> multisig
+          or a <a href="https://metamask.io/">Metamask</a>){' '}
+        </p>
+      </div>
+      <div className="text-xl mt-8">
+        <p className="mb-4">
+          <span className="font-bold">Step 4:</span> Generate a markdown button
+          for README.md's in your GitHub Org!{' '}
+        </p>
+      </div>
+    </div>
+    <div className="container mt-16 flex justify-center mx-auto pb-16">
+      <section id="flowers">
+        <pre>
+          <code> _(_)_ _(_)_ </code>
+        </pre>
+        <pre>
+          <code> @@@@ (_)@(_) @@@@ (_)@(_) </code>
+        </pre>
+        <pre>
+          <code> @@()@@ wWWWw (_)\ @@()@@ wWWWw (_)\ </code>
+        </pre>
+        <pre>
+          <code> @@@@ (___) \|/ @@@@ (___) \|/ </code>
+        </pre>
+        <pre>
+          <code> / Y \| / Y \| </code>
+        </pre>
+        <pre>
+          <code> \ | \ |/ | / \ | \ |/ | / </code>
+        </pre>
+        <pre>
+          <code> \\|// \\|/// \\\|// \\|// \\|/// \\\|// </code>
+        </pre>
+        <pre>
+          <code>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^</code>
+        </pre>
+      </section>
+    </div>
   </Layout>
-);
+)
